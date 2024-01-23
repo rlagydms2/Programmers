bbf4e617e774f9f42cf0acacc031052cec758ff7
rlagydms2 <rlagydms1@gmail.com>
2024-01-23 14:13:20 +0900

    first commit

---
+++ src/com/lec/spring/Practice1.java
@@ -0,0 +1,12 @@
+package com.lec.spring;
+
+import java.util.Scanner;
+
+public class Practice1 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String a = sc.next();
+        String b = sc.next();
+        System.out.println(a+b);
+    }
+}
